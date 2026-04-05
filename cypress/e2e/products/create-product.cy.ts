c9cf9960542613c7798258625c586e44d2be3b36
Julian Gomez Espinal <juliangoes89@hotmail.com>
2026-04-05 16:58:01 -0500

    Fix problems with create test

--- cypress/e2e/products/create-product.cy.ts
+++ cypress/e2e/products/create-product.cy.ts
@@ -14,9 +14,8 @@ describe('Create Product', () => {
 
         // Fill in the product form
         cy.get('input[id="name"]').type(product.name);
-        cy.get('input[id="price"]').type(product.price);
+        cy.get('input[id="price"]').type(product.price.toString());
         cy.get('textarea[id="description"]').type(product.description);
-        //cy.get('input[id="stock"]').clear().type(product.stock); // Set stock to 100
         // Submit the form
         cy.get('button[type="submit"]').click();
 
@@ -27,7 +26,7 @@ describe('Create Product', () => {
         cy.request('GET', 'https://api-products-efg7evbffkhybqde.brazilsouth-01.azurewebsites.net/api/products').then((response: Cypress.Response<any>) => {
             expect(response.status).to.eq(200);
             console.log('API Response:', response);
-            const createdProduct = response.body.find(p => p.name === product.name);
+            const createdProduct = response.body.find((p: any) => p.name === product.name);
             expect(createdProduct).to.exist;
             expect(createdProduct.price).to.eq(product.price);
             expect(createdProduct.description).to.eq(product.description);
